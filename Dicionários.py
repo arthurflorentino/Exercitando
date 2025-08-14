727829e89fea3245f43f66fc0729a7fbbf056e63
arthurflorentino <arthurflorentino.4@gmail.com>
2025-08-14 15:16:19 -0300

    Dicionários.py

--- Dicionários.py
+++ Dicionários.py
@@ -19,29 +19,3 @@ print('\n\n\nProdutos com valor maior que 50 reais:')
 for nome, preco in produtos.items():
     if preco > 50:
         print(f'Nome do produto: {nome}\nValor do produto: R${preco:.2f}\n')
-
-
-
-'''
-Preencha um dicionário com os dados de 5 alunos fornecidos pelo usuário.
-- Solicite os dados do usuário
-- Utilize o ra do aluno como chave e uma lista de três notas como valor.
-- Percorra o dicionário e exiba a média de cada aluno.
-Exemplo:
-Veja um exemplo da estrutura do dicionário.
-{19230490: [10, 8.0, 7.5], 2002441: [6, 5, 7.5], 2001332: [5,8,9.5]}
-'''
-
-
-'''
-Escreva uma função que conta a quantidade de vogais em um texto e armazena tal
-quantidade em um dicionário, onde a chave é a vogal considerada e o valor é a quantidade
-de vezes que essa vogal aparece no texto.
-A função deve receber o texto como entrada e retornar o dicionário.
-Exemplo:
-Para o texto abaixo:
-texto = ' faculdade impacta de tecnologia'
-A função deve devolver o seguinte dicionário:
-{'a': 5, 'u': 1, 'e': 3, 'o' : 2, 'i': 2 }
-'''
-
